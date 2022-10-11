98f4e886f301e0fb01f75814f612c548d608b342
IamChetang <96056914+IamPoojaG@users.noreply.github.com>
2022-10-12 00:43:05 +0530

    all styling part is done

--- crypto-app/src/components/InnerBottom/InnerBottom.js
+++ crypto-app/src/components/InnerBottom/InnerBottom.js
@@ -2,14 +2,147 @@ import React from 'react';
 import './InnerBottom.css';
 
 const Inner_bottom = () => {
+  const data = [
+    {
+      id: 1,
+      name: 'BITCOIN',
+      SKU: 'BTE',
+      percentage: 37,
+      deduction: -2.5,
+      color: '#F9896B',
+    },
+    {
+      id: 2,
+      name: 'DAI',
+      SKU: 'DAI',
+      percentage: 23,
+      deduction: '+7.2',
+      color: '#FFCC40',
+    },
+    {
+      id: 3,
+      name: 'RIPPLE',
+      SKU: 'XRP',
+      percentage: 20,
+      deduction: -3.1,
+      color: '#0377FB',
+    },
+    {
+      id: 4,
+      name: 'TETHER',
+      SKU: 'USDT',
+      percentage: 17,
+      deduction: '+1.8',
+      color: '#50DFB2',
+    },
+  ];
+  const dataLeft = [
+    {
+      id: 1,
+      name: 'GPUs mining',
+      status: 'Running',
+      color: '#F9896B',
+    },
+    {
+      id: 2,
+      name: 'CPUs mining',
+      status: 'Running',
+      color: '#FFCC40',
+    },
+    {
+      id: 3,
+      name: 'Ests. daily USD',
+      status: '$25.03',
+      color: '#51459E',
+    },
+    {
+      id: 4,
+      name: 'Team Members',
+      status: '12',
+      color: '#50DFB2',
+    },
+  ];
+
+  const Item = () => {
+    return (
+      <>
+        <div className='coin_inner_container flex flex_col'>
+          {data.map((ele) => (
+            <>
+              <div className='coin_sub_container flex flex_row'>
+                <div className='left_coin flex '>
+                  <div
+                    key={ele.id}
+                    style={{ backgroundColor: `${ele.color}` }}
+                    className='coin_image'
+                  ></div>
+                  <div key={ele.id} className='coin_name'>
+                    <span style={{ fontSize: '1rem' }}>{ele.name}</span>
+                    <br />
+                    <span>{ele.SKU}</span>
+                  </div>
+                </div>
+                <div className='right'>
+                  <div key={ele.id} className='coin_percentage'>
+                    <span>{ele.percentage}%</span>
+                    <br />
+                    <span style={{ fontSize: '0.7rem' }}>{ele.deduction}%</span>
+                  </div>
+                </div>
+              </div>
+            </>
+          ))}
+        </div>
+      </>
+    );
+  };
+
+  const ItemRight = () => {
+    return (
+      <>
+        <div className='coin_inner_container flex flex_col'>
+          {dataLeft.map((ele) => (
+            <>
+              <div className='coin_sub_container flex flex_row color_black'>
+                <div className='left_coin flex '>
+                  <div
+                    key={ele.id}
+                    style={{ backgroundColor: `${ele.color}` }}
+                    className='coin_image'
+                  ></div>
+                  <div key={ele.id} className='coin_name'>
+                    <span style={{ fontSize: '1rem' }}>{ele.name}</span>
+                    <br />
+                    <span>{ele.status}</span>
+                  </div>
+                </div>
+              </div>
+            </>
+          ))}
+        </div>
+      </>
+    );
+  };
+
   return (
     <>
       <div className='left_container flex absolute_centers'>
-        <div className='heading'></div>
+        <div className='heading flex flex-row'>
+          <h5>My Wallet</h5>
+          <p className='view'>View all</p>
+        </div>
+        <div className='coin_container'>
+          <Item />
+        </div>
       </div>
 
-      <div className='right_container flex absolute_centers'>
-        <div> left</div>
+      <div className='right_container flex absolute_center flex_col'>
+        <div className='heading flex flex-row'>
+          <h5>Mining Status</h5>
+        </div>
+        <div className='coin_container'>
+          <ItemRight />
+        </div>
       </div>
     </>
   );
--- crypto-app/src/components/Main/Main.js
+++ crypto-app/src/components/Main/Main.js
@@ -3,6 +3,11 @@ import './Main.css';
 
 import { MdOutlineNotificationsNone } from 'react-icons/md';
 import { BiMessageSquareDetail } from 'react-icons/bi';
+import { SiMastercard } from 'react-icons/si';
+import { AiOutlinePlusCircle } from 'react-icons/ai';
+import { BsDownload } from 'react-icons/bs';
+import graphImage from '../../common/Resource/graphImage.png';
+import Internet from '../../common/Resource/Capture.JPG';
 import InnerBottom from '../InnerBottom/InnerBottom';
 
 const Main = () => {
@@ -39,12 +44,126 @@ const Main = () => {
         </nav>
         <div className='content_container flex absolute_centers flex_row'>
           <div className='mid_content_container flex absolute_centers flex_col'>
-            <div className='inner_top_container'></div>
+            <div className='inner_top_container flex absolute_centers flex_col'>
+              <div className='sub_name_container flex flex_row'>
+                <h5>Income</h5>
+                <div className='sorting_container flex flex_rpw'>
+                  <label>Sort By:</label>
+                  <select>
+                    <option>Day </option>
+                    <option>Week </option>
+                    <option>Month </option>
+                    <option>Year </option>
+                    <option>All </option>
+                  </select>
+                  <span>
+                    <BsDownload />
+                  </span>
+                </div>
+              </div>
+              <div className='graph'>
+                <img src={graphImage} alt='graphImage' />
+              </div>
+              <div className='time_container flex absolute_centers flex_row'>
+                <ul className='flex flex_row'>
+                  <li>Apr01</li>
+                  <li>Apr02</li>
+                  <li>Apr03</li>
+                  <li>Apr04</li>
+                  <li>Apr05</li>
+                  <li>Apr06</li>
+                  <li>Apr07</li>
+                </ul>
+              </div>
+              <div className='filter_container flex absolute_centers flex_row'>
+                <ul className='flex flex_row'>
+                  <li>1d</li>
+                  <li>1w</li>
+                  <li>1m</li>
+                  <li>3m</li>
+                  <li>1y</li>
+                  <li>all</li>
+                </ul>
+              </div>
+            </div>
             <div className='inner_bottom_container flex absolute_centers flex_row'>
               <InnerBottom />
             </div>
           </div>
-          <div className='right_content_container flex flex_col'></div>
+          <div className='right_content_container flex flex_col'>
+            <h5>Send Money</h5>
+            <div className='card flex flex_row btn'>
+              <h5>
+                <span style={{ marginRight: '0.6rem' }}>
+                  <SiMastercard />
+                </span>{' '}
+                Debit
+              </h5>
+              <select>
+                <option>$10,680</option>
+                <option>$9,280</option>
+                <option>$9,980</option>
+              </select>
+            </div>
+            <div className='input_container'>
+              <div class='form-floating'>
+                <input
+                  type='number'
+                  class='form-control'
+                  id='floatingPassword'
+                  placeholder='Enter the amount'
+                />
+                <label for='floatingPassword'>
+                  <img src={src} alt='input img' />
+                  &nbsp;&nbsp;&nbsp;&nbsp;Enter the amount
+                </label>
+              </div>
+            </div>
+            <div className='send_container flex flex_col'>
+              <div className='inner_send flex flex_row'>
+                <h5>
+                  <img src={src} alt='input img' />
+                  &nbsp;&nbsp; Astrid Hayes
+                </h5>
+                <span>
+                  <AiOutlinePlusCircle />
+                </span>
+              </div>
+              <div className='btn btn_send'>Send Money</div>
+            </div>
+            <div className='income_container flex flex_row'>
+              <h5>Internet Speed</h5>
+              <p>view all</p>
+            </div>
+            <div>
+              <img src={Internet} alt='Internet' />
+            </div>
+            <div>
+              <h6>Last speed tests</h6>
+            </div>
+            <div className='internet_bottom_container flex  flex_row'>
+              <ul className='flex  flex_col'>
+                <li>Date</li>
+                <li>Sep 1,01:12 AM</li>
+                <li>Sep 1,10:12 AM</li>
+                <li>Sep 1,08:12 AM</li>
+              </ul>
+              <ul className='flex flex_col'>
+                <li>Download</li>
+
+                <li>75.4</li>
+                <li>72.4</li>
+                <li>73.3</li>
+              </ul>
+              <ul className='flex  flex_col'>
+                <li>Upload</li>
+                <li>67.5</li>
+                <li>67.3</li>
+                <li>67.5</li>
+              </ul>
+            </div>
+            ;
+          </div>
         </div>
       </div>
     </>
